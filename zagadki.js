7de23398ee3b47348f535a166aa1114b8c047930
Matix14 <66333467+Matix14@users.noreply.github.com>
2021-05-17 10:48:02 +0200

    Add files via upload

---
+++ zagadki.js
@@ -0,0 +1,53 @@
+function z1()
+{
+     document.getElementById("z1").innerHTML="Wszystkie! Każdy miesiąc ma co najmniej 28 dni.";
+}
+
+function z2()
+    {
+        document.getElementById("z1").innerHTML="Tu znajdziesz odpowiedź";
+    } 
+
+function z3()
+{
+     document.getElementById("z3").innerHTML="7 - A L F A B E T";
+}
+    
+function z4()
+    {
+        document.getElementById("z3").innerHTML="Tu znajdziesz odpowiedź";
+    }   
+        
+
+
+function z5()
+{
+     document.getElementById("z5").innerHTML="Twoje imię.";
+}
+        
+function z6()
+    {
+        document.getElementById("z5").innerHTML="Tu znajdziesz odpowiedź";
+    }
+
+
+function z7()
+{
+     document.getElementById("z7").innerHTML="Wszystkie zwierzęta są albo na łóżku albo latają  więc jedyne nogi na ziemi są to twoje nogi i nogi łóżka łącznie 6 nóg.";
+}
+
+function z8()
+    {
+        document.getElementById("z7").innerHTML="Tu znajdziesz odpowiedź";
+    }
+
+
+    function z9()
+{
+     document.getElementById("z9").innerHTML="Czy już śpisz?";
+}
+
+function z10()
+    {
+        document.getElementById("z9").innerHTML="Tu znajdziesz odpowiedź";
+    }
